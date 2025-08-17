382f5c7701718f8b21c55f5d696dd094ad2dc343
Logan Minarik <168377908+LoganMinarik@users.noreply.github.com>
2025-08-17 11:42:06 -0400

    Update inputTextTrimming.js

--- javaScriptCrashCourse/inputTextTrimming.js
+++ javaScriptCrashCourse/inputTextTrimming.js
@@ -2,3 +2,9 @@ let inputText = " Here is my input text.       ";
 console.log(inputText.trim());
 // Output: "Here is my input text."
 console.log(inputText);
+
+let textToLowerCase = "This is a Sample Text.";
+console.log(textToLowerCase.toLowerCase());
+// Output: "this is a sample text."
+console.log(textToLowerCase);
+
